83491f9e3926a27fb0fe3bea7c3d380db474ee50
zifeng-w <895924794@qq.com>
2020-12-31 18:04:43 +0800

    第三章：数组

---
+++ src/zifeng/chapter03/ArrayNote.java
@@ -0,0 +1,110 @@
+package zifeng.chapter03;
+
+/**
+ *   数组：指的就是一组相关变量的组合;
+ *
+ *   数组的定义语法：
+ *     第一种：声明并开辟数组
+ *      数据类型 数据名称[] = new 数据类型[长度];
+ *      数据类型[] 数组名称 = new 数据类型[长度] ;
+ *
+ *     第二种；
+ *      声明数组： 数据类型  数组名称[长度] = null ;
+ *      开辟数组： 数组名称  = new 数据类型[长度];
+ *
+ *      所有数组的下标从0开始，若超过数组允许下表的长度，会出现数组越界异常(Array Index Out Of Bounds Exception)
+ *
+ *      数组.length
+ *
+ *
+ *      数组本身属于引用数据类型，如果直接使用未开辟空间的数组，一定会出现"NullPointerException"（空指向异常）
+ *
+ *
+ *      二维数组
+ *        动态初始化： 数据类型 数组名称[][] = new 数据类型[行的个数][列的个数];
+ *        静态初始化： 数据类型 数组名称[][] = new 数据类型[][]{{值，值，值}{值，值，值}}
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+public class ArrayNote {
+    public static void main(String[] args) {
+        int arr[][] = new int[][]{{1,2,3},{4,5,6},{7,8,9}}; //定义二维数组
+        for (int x = 0 ; x < arr.length;x++){
+            for (int i = 0; i < arr[x].length ; i++) {
+                System.out.println(arr[x][i]+"\t");
+                //System.out.println(arr);  //输出的是地址
+            }
+            System.out.println();
+        }
+
+        System.out.println("*************");
+
+        int arr1[] = new int[]{1,2,4};
+
+
+        change(arr1);
+        for (int i = 0; i <arr1.length ; i++) {
+            System.out.println(arr1[i]);
+        }
+
+        System.out.println("****************");
+        //数组排序
+        int arrSort[] = new int[]{8,6,34,1,6,7,2};
+        for (int i = 0; i < arrSort.length; i++) {
+            System.out.print(arrSort[i] + "\t");
+
+        }
+        System.out.println();
+        sort(arrSort);
+        for (int x = 0; x < arrSort.length; x++){
+            System.out.print(arrSort[x]+"\t");
+
+        }
+
+
+    }
+
+    /**
+     * 此方法的主要功能是进行数组数据的改变操作，将数组中的每个元素乘以2
+     * @param temp 要进行改变内容的数组引用
+     */
+    public static void change(int temp[]){
+        for (int i = 0; i< temp.length;i++){
+            temp[i] *= 2;
+        }
+    }
+
+    /**
+     * 此方法的主要是功能是进行数组排序，按照数值从大到小进行排序
+     * @param temp
+     */
+    public static void  sort(int temp[]){
+        for (int i = 0; i <temp.length ; i++) {
+            for (int j = 0; j < temp.length-1; j++) {
+                if (temp[j] <temp[j+1]) {
+
+                    int t =temp[j];
+                    temp[j] = temp[j+1];
+                    temp[j+1] =t;
+
+                }
+
+            }
+
+
+        }
+    }
+
+
+
+}
+
+
--- src/zifeng/chapter03/ClassNote.java
+++ src/zifeng/chapter03/ClassNote.java
@@ -1,7 +1,14 @@
 package zifeng.chapter03;
 
 /**
- *  定义类测试
+ *  简单Java类
+ *
+ *   基本开发要求
+ *      类名称必须存在意义
+ *      类中所有的属性必须private封装，封装后的属性必须提供setter,getter
+ *      类中可以提供任意多个构造方法，但是必须保留一个无参构造方法
+ *      类中不允许出现任何输出的语句，所有信息输出必须交给被调用处输出
+ *      类中需要提供有一个取得对象完整信息的方法，暂定为getInfo()，而且返回String数据;
  *
  *
  *
@@ -9,5 +16,81 @@ package zifeng.chapter03;
  */
 public class ClassNote {
 
+    public static void main(String[] args) {
+         Emp e = new Emp(123,"test","经理",1000,888);
+        System.out.println(e.getInfo());
+
+    }
+}
+
+class Emp{
+    private int empno;
+    private String ename;
+    private  String job;
+    private double sal;
+    private double comm;
+    public  Emp(){
+
+    }
+    public Emp(int eno,String ena,String jobs,double sals,double comms){
+        empno = eno;
+        ename = ena;
+        job = jobs;
+        sal = sals;
+        comm = comms;
+    }
+
+    public int getEmpno() {
+        return empno;
+    }
+
+    public void setEmpno(int empno) {
+        this.empno = empno;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public double getSal() {
+        return sal;
+    }
+
+    public void setSal(double sal) {
+        this.sal = sal;
+    }
+
+    public double getComm() {
+        return comm;
+    }
+
+    public void setComm(double comm) {
+        this.comm = comm;
+    }
+
+    /**
+     * 取得简单Java类的基本信息，信息在被调用处输出
+     * @return 包含对象完整信息的字符串数据
+     */
+    public String getInfo(){
+       return "雇员编号" + "\n" +
+               "雇员姓名" + "\n" +
+               "雇员职位" + "\n" +
+               "基本工资" + "\n" +
+               "佣金" + "\n" ;
+
+    }
 }
 
